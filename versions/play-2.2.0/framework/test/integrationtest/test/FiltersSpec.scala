2884ef47c628d92f3f29b87fa555ed8cce72875e
Dennis Vis <dennisvis86@gmail.com>
2013-10-12 15:42:44 +0200

    Updating to play-2.2.0

---
+++ versions/play-2.2.0/framework/test/integrationtest/test/FiltersSpec.scala
@@ -0,0 +1,60 @@
+package test
+
+import org.specs2.mutable.Specification
+import play.api.mvc._
+import play.api.Routes
+import play.api.test._
+import play.api.test.Helpers._
+import scala.concurrent.duration.Duration
+import scala.concurrent._
+
+object FiltersSpec extends Specification {
+  "filters should" should {
+    "be able to access request tags" in {
+
+      object MockGlobal extends WithFilters(Filter { (f, rh) =>
+        Future.successful(rh.tags.get(Routes.ROUTE_VERB).map(verb => Results.Ok(verb)).getOrElse(Results.NotFound))
+      })
+
+      "helpers routing" in new WithApplication(FakeApplication(withGlobal = Some(MockGlobal))) {
+        val result = route(FakeRequest("GET", "/")).get
+        status(result) must_== 200
+        contentAsString(result) must_== "GET"
+      }
+
+      "a filter can access request tags" in new WithServer(FakeApplication(withGlobal = Some(MockGlobal))) {
+        val response = Await.result(wsCall(controllers.routes.Application.index()).get(), Duration.Inf)
+        response.status must_== 200
+        response.body must_== "GET"
+      }
+
+      object MockGlobal2 extends play.api.GlobalSettings {
+
+        override def doFilter(next: RequestHeader => Handler): (RequestHeader => Handler) = {
+          rh => {
+            rh.tags.get("ROUTE_CONTROLLER").collect {
+              case "controllers.Application" => next(rh)
+            }.getOrElse {
+              Action {
+                Results.BadRequest("Unexpected path!")
+              }
+            }
+          }
+        }
+
+      }
+
+      "running server" in new WithServer(FakeApplication(withGlobal = Some(MockGlobal2))) {
+        val response = Await.result(wsCall(controllers.routes.Application.plainHelloWorld()).get(), Duration.Inf)
+        response.status must_== 200
+        response.body must_== ("Hello World")
+
+        val response2 = Await.result(wsCall(controllers.routes.JavaApi.index()).get(), Duration.Inf)
+        response2.status must_== 400
+        response2.body must_== ("Unexpected path!")
+      }
+
+
+    }
+  }
+}
